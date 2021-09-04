95c3aa2d2003e044ecde409d9ea33ee756e638c7
gomezteo1 <atehortuamateo123@gmail.com>
2021-09-04 00:17:35 -0500

    04-09-2021 0-17 creando el personaje

---
+++ Assets/_Game/_gomezteo1/Scripts/SeguirPersonaje.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirPersonaje : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Jugador;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = Jugador.transform.position + new Vector3(-1828.30005f, 25, -820.880005f);
+        //transform.position = Jugador.transform.position + new Vector3(-1820, 23, -819);
+    }
+}
